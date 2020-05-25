f67e1e4931f57c2628415eb833ab3fed4238f27f
hkghkg <599098339@qq.com>
2020-05-25 11:12:35 +0800

    新建了组件C

---
+++ gitvue/src/components/C.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="c_two">
+        这是ProjectTwo创建的C组件
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "C"
+    }
+</script>
+
+<style scoped>
+
+</style>
