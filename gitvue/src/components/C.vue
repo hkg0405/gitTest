ea2c33f794dbfe0fcca9175a8b876afc06af71ab
hkghkg <599098339@qq.com>
2020-05-25 11:14:25 +0800

    新建了C组件

---
+++ gitvue/src/components/C.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="c_one">
+        这是ProjectOne创建的c组件
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "C"
+    }
+</script>
+
+<style scoped>
+
+</style>
